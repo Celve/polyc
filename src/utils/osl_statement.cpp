50fd3b7ff1985bacdb6ecf9343726f80cc8dd130
Celve <celve03@gmail.com>
2023-05-08 12:14:42 +0800

    chore: remove one debug info

--- src/utils/osl_statement.cpp
+++ src/utils/osl_statement.cpp
@@ -5,7 +5,6 @@ osl_statement_p NavigateOslStmt(osl_statement_p stmt,
                                 std::vector<int> stmt_id) {
   while (stmt != nullptr && ScatteringMatrix(stmt->scattering) <= stmt_id) {
     stmt = stmt->next;
-    std::cout << "next" << std::endl;
   }
   return stmt;
 }
